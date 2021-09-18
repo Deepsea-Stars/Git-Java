9a046cb3b565a0685d28377675225a8f0bef21e8
Dreamer <1938478963@qq.com>
2021-09-18 12:35:43 +0800

    master

---
+++ src/AppDemo.java
@@ -0,0 +1,10 @@
+/*
+    测试文件
+    同一个文件夹中不得有两个同类名的Java源文件
+*/
+public class AppDemo {
+    public static void main(String[] args) {
+        System.out.println("This is my first App!");
+    }
+
+}
